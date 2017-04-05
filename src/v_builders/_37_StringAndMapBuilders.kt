84fd417eb64a8fa04a34c95bc8790077d1e84f88
Evan Leis <evan.explodes@gmail.com>
2017-04-04 21:53:55 -0600

    [v_builders] kotlin is not a pretty language...

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -20,7 +20,7 @@ fun buildStringExample(): String {
 }
 
 fun todoTask37(): Nothing = TODO(
-    """
+        """
         Task 37.
         Uncomment the commented code and make it compile.
         Add and implement function 'buildMap' with one parameter (of type extension function) creating a new HashMap,
@@ -28,12 +28,18 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
+fun <K, V> buildMap(build: HashMap<K, V>.() -> Unit): HashMap<K, V> {
+    val map = HashMap<K, V>()
+    map.build()
+    //build(map)
+    return map
+}
+
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
